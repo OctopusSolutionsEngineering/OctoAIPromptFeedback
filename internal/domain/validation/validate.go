21cc51558d5ee3117c08b20c71bcf2e470cdfdc7
Matthew Casperson <matthewcasperson@gmail.com>
2025-04-09 17:10:04 +1000

    Server is required

--- internal/domain/validation/validate.go
+++ internal/domain/validation/validate.go
@@ -3,11 +3,16 @@ package validation
 import (
 	"errors"
 	"github.com/OctopusSolutionsEngineering/OctoAIPromptFeedback/internal/domain/model"
+	"strings"
 )
 
 func ValidateFeedback(feedback model.Feedback) error {
-	if feedback.ID == "" {
+	if strings.TrimSpace(feedback.ID) == "" {
 		return errors.New("id is required")
+	}
+
+	if strings.TrimSpace(feedback.Server) == "" {
+		return errors.New("server is required")
 	}
 
 	return nil
