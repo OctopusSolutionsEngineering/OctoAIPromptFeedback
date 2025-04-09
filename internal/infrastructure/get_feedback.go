43fffca4ee6e7759f867b7c6f576d56943c61477
Matthew Casperson <matthewcasperson@gmail.com>
2025-04-09 16:20:28 +1000

    Added ability to get single respons

--- internal/infrastructure/get_feedback.go
+++ internal/infrastructure/get_feedback.go
@@ -3,7 +3,6 @@ package infrastructure
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/Azure/azure-sdk-for-go/sdk/data/aztables"
 	"github.com/OctopusSolutionsEngineering/OctoAIPromptFeedback/internal/domain/model"
 	"time"
@@ -29,14 +28,11 @@ func GetFeedback() ([]model.Feedback, error) {
 
 	retValue := []model.Feedback{}
 
-	pageCount := 0
 	for pager.More() {
 		response, err := pager.NextPage(ctx)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
-		fmt.Printf("There are %d entities in page #%d\n", len(response.Entities), pageCount)
-		pageCount += 1
 
 		for _, entity := range response.Entities {
 			var myEntity aztables.EDMEntity
@@ -60,11 +56,49 @@ func GetFeedback() ([]model.Feedback, error) {
 }
 
 func GetFeedbackItem(id string) (model.Feedback, bool, error) {
-	return model.Feedback{
-		ID:        "0",
-		Timestamp: time.Now(),
-		Prompt:    "Sample prompt",
-		Comment:   "This is a sample comment",
-		ThumbsUp:  false,
-	}, true, nil
+	service, err := aztables.NewServiceClientFromConnectionString(GetStorageConnectionString(), nil)
+
+	if err != nil {
+		return model.Feedback{}, false, err
+	}
+
+	ctx := context.Background()
+
+	if err := CreateTable(service, ctx); err != nil {
+		return model.Feedback{}, false, err
+	}
+
+	client := service.NewClient("Feedback")
+
+	filter := "RowKey eq '" + id + "'"
+	options := &aztables.ListEntitiesOptions{
+		Filter: &filter,
+	}
+	pager := client.NewListEntitiesPager(options)
+
+	for pager.More() {
+		response, err := pager.NextPage(ctx)
+		if err != nil {
+			return model.Feedback{}, false, err
+		}
+
+		for _, entity := range response.Entities {
+			var myEntity aztables.EDMEntity
+			err = json.Unmarshal(entity, &myEntity)
+			if err != nil {
+				return model.Feedback{}, false, err
+			}
+
+			return model.Feedback{
+				ID:        myEntity.RowKey,
+				Timestamp: GetTimeProperty("Timestamp", myEntity, time.Time{}),
+				Server:    GetStringProperty("Server", myEntity, ""),
+				Prompt:    GetStringProperty("Prompt", myEntity, ""),
+				Comment:   GetStringProperty("Comment", myEntity, ""),
+				ThumbsUp:  GetBoolProperty("Server", myEntity, false),
+			}, true, nil
+		}
+	}
+
+	return model.Feedback{}, false, nil
 }
