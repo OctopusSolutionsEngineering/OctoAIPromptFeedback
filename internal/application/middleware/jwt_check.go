d84b73b0ef053a2a1c10a6b7785a0cdf64252d63
Matthew Casperson <matthewcasperson@gmail.com>
2025-10-13 11:42:30 +1000

    Fix up auth checks to allow custom server url.

--- internal/application/middleware/jwt_check.go
+++ internal/application/middleware/jwt_check.go
@@ -3,55 +3,74 @@ package middleware
 import (
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/client"
 	"github.com/OctopusSolutionsEngineering/OctoAIPromptFeedback/internal/application/responses"
-	"github.com/OctopusSolutionsEngineering/OctoAIPromptFeedback/internal/domain/jwt"
+	"github.com/OctopusSolutionsEngineering/OctoAIPromptFeedback/internal/domain/router"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"net/http"
 	"net/url"
 	"strings"
 )
 
-// JwtCheck is a middleware function that checks for a valid JWT token in the request header.
-func JwtCheck(c *gin.Context) {
+func JwtCheckMiddleware(skipValidation bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
 
-	// At the end of the day, this service is essentially unauthenticated.
-	// We accept any user with a valid JWT token that appears to authenticate with an Octopus Deploy instance.
-	token := strings.TrimPrefix(c.GetHeader("Authorization"), "Bearer ")
+		// At the end of the day, this service is essentially unauthenticated.
+		// We accept any user with a valid JWT token that appears to authenticate with an Octopus Deploy instance.
+		token := strings.TrimSpace(strings.TrimPrefix(c.GetHeader("Authorization"), "Bearer "))
+		server := c.GetHeader("X-Octopus-Url")
 
-	aud, err := jwt.ValidateJWT(token)
+		if token == "" {
+			// If the token is empty, we don't need to do anything
+			c.Next()
+			return
+		}
 
-	if err != nil {
-		c.IndentedJSON(http.StatusUnauthorized, responses.GenerateError("Failed to process request", err))
-		c.Abort()
-		return
+		apiURL, err := url.Parse(server)
+		if err != nil {
+			zap.L().Error("Failed to process request", zap.Error(err))
+			c.IndentedJSON(http.StatusUnauthorized, responses.GenerateError("Failed to process request", err))
+			c.Abort()
+			return
+		}
+
+		// Don't try to validate the token with an API call
+		if skipValidation {
+			c.Next()
+			return
+		}
+
+		routerClient, host, err := router.GetHttpClient(apiURL)
+
+		if err != nil {
+			zap.L().Error("Failed to create HTTP client", zap.Error(err))
+			c.IndentedJSON(http.StatusInternalServerError, responses.GenerateError("Failed to process request", err))
+			c.Abort()
+			return
+		}
+
+		// Use the token to look up the user. This is not foolproof - you could supply any valid JWT token
+		// with an audience claim that points to a server that responds to this API request.
+		// We can't prove that anyone submitting feedback is a genuine Octopus user.
+		// But we do effectively prove that you own a DNS name, which is almost as good.
+		// Since we store the audience in the feedback items, we can filter out bad requests later.
+		// It also raises the bar for anyone looking to abuse the API, as you would need to generate valid JWTs,
+		// host a JWKS server, and host a server that responds the API request.
+		octopusClient, err := client.NewClientWithAccessToken(routerClient, host, token, "")
+		if err != nil {
+			zap.L().Error("Failed to open Octopus client", zap.Error(err))
+			c.IndentedJSON(http.StatusInternalServerError, responses.GenerateError("Failed to process request", err))
+			c.Abort()
+			return
+		}
+
+		if _, err := octopusClient.Users.GetMe(); err != nil {
+			zap.L().Error("Failed to contact Octopus me endpoint", zap.Error(err))
+			c.IndentedJSON(http.StatusUnauthorized, responses.GenerateError("Failed to process request", err))
+			c.Abort()
+			return
+		}
+
+		// normal request, and the execution chain is called down
+		c.Next()
 	}
-
-	apiURL, err := url.Parse(aud)
-	if err != nil {
-		c.IndentedJSON(http.StatusUnauthorized, responses.GenerateError("Failed to process request", err))
-		c.Abort()
-		return
-	}
-
-	// Use the token to look up the user. This is not foolproof - you could supply any valid JWT token
-	// with an audience claim that points to a server that responds to this API request.
-	// We can't prove that anyone submitting feedback is a genuine Octopus user.
-	// But we do effectively prove that you own a DNS name, which is almost as good.
-	// Since we store the audience in the feedback items, we can filter out bad requests later.
-	// It also raises the bar for anyone looking to abuse the API, as you would need to generate valid JWTs,
-	// host a JWKS server, and host a server that responds the API request.
-	octopusClient, err := client.NewClientWithAccessToken(nil, apiURL, token, "")
-	if err != nil {
-		c.IndentedJSON(http.StatusInternalServerError, responses.GenerateError("Failed to process request", err))
-		c.Abort()
-		return
-	}
-
-	if _, err := octopusClient.Users.GetMe(); err != nil {
-		c.IndentedJSON(http.StatusUnauthorized, responses.GenerateError("Failed to process request", err))
-		c.Abort()
-		return
-	}
-
-	// normal request, and the execution chain is called down
-	c.Next()
 }
---
+++ internal/domain/router/client.go
@@ -0,0 +1,95 @@
+package router
+
+import (
+	"errors"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/OctopusSolutionsEngineering/OctoAIPromptFeedback/internal/domain/environment"
+	"github.com/samber/lo"
+	"go.uber.org/zap"
+)
+
+type HeaderRoundTripper struct {
+	Transport http.RoundTripper
+	Headers   map[string]string
+}
+
+func (h *HeaderRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	for key, value := range h.Headers {
+		req.Header.Set(key, value)
+	}
+	return h.Transport.RoundTrip(req)
+}
+
+func GetHttpClient(octopusUrl *url.URL) (*http.Client, *url.URL, error) {
+	if !isDirectlyAccessibleOctopusInstance(octopusUrl) {
+		zap.L().Info("Enabled Octopus AI Assistant redirection service")
+		return createHttpClient(octopusUrl)
+	}
+
+	zap.L().Info("Did not enable Octopus AI Assistant redirection service")
+
+	return nil, octopusUrl, nil
+}
+
+// isDirectlyAccessibleOctopusInstance determines if the host should be contacted directly
+func isDirectlyAccessibleOctopusInstance(octopusUrl *url.URL) bool {
+	serviceEnabled, found := os.LookupEnv("REDIRECTION_SERVICE_ENABLED")
+
+	if !found || serviceEnabled != "true" {
+		return true
+	}
+
+	bypassList := environment.GetRedirectionBypass()
+
+	// Allow bypassing specific domains via environment variable
+	if lo.Contains(bypassList, octopusUrl.Hostname()) {
+		return true
+	}
+
+	// Allow forcing all traffic through the redirection service
+	if environment.GetRedirectionForce() {
+		return false
+	}
+
+	return strings.HasSuffix(octopusUrl.Hostname(), ".octopus.app") ||
+		strings.HasSuffix(octopusUrl.Hostname(), ".testoctopus.com") ||
+		octopusUrl.Hostname() == "localhost" ||
+		octopusUrl.Hostname() == "127.0.0.1"
+}
+
+func createHttpClient(octopusUrl *url.URL) (*http.Client, *url.URL, error) {
+
+	serviceApiKey, found := os.LookupEnv("REDIRECTION_SERVICE_API_KEY")
+
+	if !found {
+		return nil, nil, errors.New("REDIRECTION_SERVICE_API_KEY is required")
+	}
+
+	redirectionHost, found := os.LookupEnv("REDIRECTION_HOST")
+
+	if !found {
+		return nil, nil, errors.New("REDIRECTION_HOST is required")
+	}
+
+	redirectionHostUrl, err := url.Parse("https://" + redirectionHost)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	headers := map[string]string{
+		"X_REDIRECTION_UPSTREAM_HOST":   octopusUrl.Hostname(),
+		"X_REDIRECTION_SERVICE_API_KEY": serviceApiKey,
+	}
+
+	return &http.Client{
+		Transport: &HeaderRoundTripper{
+			Transport: http.DefaultTransport,
+			Headers:   headers,
+		},
+	}, redirectionHostUrl, nil
+}
